b34f63659317901e7b6b989fda16effcc0355df1
wsoc <wilsonmoralesofficial@gmail.com>
2026-06-21 16:43:55 -0600

    Reestructuración de archivos

--- mi-api/test_app.py
+++ mi-api/test_app.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_root():
     r = client.get("/")
     assert r.status_code == 200
-    assert r.json() == {"mensaje": "Prueba Kubernetes "}
+    assert r.json() == {"mensaje": "Mi primera API containerizada"}
 
 def test_salud():
     r = client.get("/salud")
